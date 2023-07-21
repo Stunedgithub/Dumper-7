6fffa5a3b3a1c8f9bdadcccf4a4ce15bbee78b57
Fischsalat <64608145+Fischsalat@users.noreply.github.com>
2023-07-21 16:36:30 +0200

    I'm stupid.

--- Dumper/UnrealObjects.cpp
+++ Dumper/UnrealObjects.cpp
@@ -903,7 +903,7 @@ std::string UELazyObjectProperty::GetCppType() const
 std::string UESoftObjectProperty::GetCppType() const
 {
 	if (!GetPropertyClass())
-		return std::format("TSoftObjectPtr<class UObject>", GetPropertyClass().GetCppName());
+		return "TSoftObjectPtr<class UObject>";
 
 	return std::format("TSoftObjectPtr<class {}>", GetPropertyClass().GetCppName());
 }
